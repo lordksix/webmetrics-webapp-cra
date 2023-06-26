0fefdf250f8fa690145dd53031c66d5507fc03bd
Wladimir Pasquel <wladimir.pasquel@urp.edu.pe>
2023-06-26 17:45:02 -0500

    Create Header.jsx

---
+++ src/componets/Header.jsx
@@ -0,0 +1,66 @@
+import { nanoid } from '@reduxjs/toolkit';
+import { Link, NavLink, useLocation } from 'react-router-dom';
+import logo from 'images/lordksix-logos_transparent.png';
+import { GiHamburgerMenu } from 'react-icons/gi';
+import SearchBox from './SearchBox';
+
+const links = [
+  { path: 'current', text: 'Current' },
+  { path: 'forecast', text: 'Forecast' },
+  { path: 'Historical', text: 'Historical' },
+  { path: '/', text: 'Home' },
+];
+
+const Header = () => {
+  const location = useLocation();
+
+  const mobileBtn = (
+    <Link
+      to="/modal"
+      state={{ previousLocation: location }}
+      className="navbarBtn"
+    >
+      <GiHamburgerMenu />
+    </Link>
+  );
+
+  const navbar = (
+    links.map((link) => (
+      <li key={nanoid()}>
+        <NavLink
+          to={link.path}
+          className={({ isActive }) => (isActive ? 'active' : undefined)}
+        >
+          {link.text}
+        </NavLink>
+      </li>
+    ))
+  );
+
+  return (
+    <header className="stickyHeader">
+      <div className="headerLogo">
+        <img
+          src={logo}
+          alt="Air Quality App"
+          style={{
+            height: '25px',
+            width: 'auto',
+          }}
+        />
+        <h1 className="h1">Air Quality App</h1>
+      </div>
+      {mobileBtn}
+      <div className="full-header">
+        <SearchBox />
+        <nav aria-label="main">
+          <ul className="full-navbar">
+            {navbar}
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
