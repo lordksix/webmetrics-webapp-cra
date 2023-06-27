1d64de7fec08c0c34747f0a65fae6af4195a2571
Wladimir Pasquel <wladimir.pasquel@urp.edu.pe>
2023-06-26 20:56:00 -0500

    Create MenuModal.jsx

---
+++ src/componets/MenuModal.jsx
@@ -0,0 +1,87 @@
+import { nanoid } from '@reduxjs/toolkit';
+import { useEffect, useRef } from 'react';
+import { Link, NavLink, useLocation } from 'react-router-dom';
+import { disableBodyScroll, enableBodyScroll } from 'body-scroll-lock';
+import { GiExitDoor } from 'react-icons/gi';
+import logo from 'images/lordksix-logos_transparent.png';
+import SearchBox from './SearchBox';
+import 'styles/MenuModal.css';
+
+const MenuModal = () => {
+  const modalRef = useRef();
+  const location = useLocation();
+
+  useEffect(() => {
+    const observerRefValue = modalRef.current;
+    disableBodyScroll(observerRefValue);
+
+    return () => {
+      if (observerRefValue) {
+        enableBodyScroll(observerRefValue);
+      }
+    };
+  }, []);
+
+  const links = [
+    { path: '/current', text: 'Current' },
+    { path: '/forecast', text: 'Forecast' },
+    { path: '/historical', text: 'Historical' },
+    { path: '/', text: 'Home' },
+  ];
+
+  const navbar = (
+    links.map((link) => (
+      <li
+        key={nanoid()}
+        className={`sqMenu${(location?.state?.previousLocation?.pathname === link.path) ? ' modalActive' : ''}`}
+      >
+        <NavLink
+          to={link.path}
+          className="sqAnchor"
+        >
+          <span className={`${(location?.state?.previousLocation?.pathname === link.path) ? 'active' : undefined}`}>{link.text}</span>
+        </NavLink>
+      </li>
+    ))
+  );
+  const mobileBtn = (
+    <Link
+      to={location?.state?.previousLocation?.pathname || '/'}
+      className="navbarBtn"
+    >
+      <button type="button" className="flex-align">
+        <GiExitDoor />
+      </button>
+    </Link>
+  );
+  return (
+    <section className="modalMenuWrapper" ref={modalRef}>
+      <div className="modalMenu">
+        <div className="modalTitle">
+          <div className="headerLogo">
+            <img
+              src={logo}
+              alt="Air Quality App"
+              style={{
+                height: '25px',
+                width: 'auto',
+              }}
+            />
+            <h1 className="h1">Air Quality</h1>
+          </div>
+          {mobileBtn}
+        </div>
+        <div className="modalBody">
+          <SearchBox />
+          <nav aria-label="mobile" className="modalNavbar">
+            <ul>
+              {navbar}
+            </ul>
+          </nav>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default MenuModal;
