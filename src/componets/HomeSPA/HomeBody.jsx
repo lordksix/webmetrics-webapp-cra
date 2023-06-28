ba5a095e636a718d14b3204eff69b5623c124b5c
Wladimir Pasquel <wladimir.pasquel@urp.edu.pe>
2023-06-27 21:16:55 -0500

    Implement Current Air Quality

--- src/componets/HomeSPA/HomeBody.jsx
+++ src/componets/HomeSPA/HomeBody.jsx
@@ -1,7 +1,9 @@
+import { nanoid } from '@reduxjs/toolkit';
 import LocationBtn from 'componets/CommomComponents/LocationBtn';
 import SearchBox from 'componets/CommomComponents/SearchBox';
 import { selectAirData } from 'features/AirPollution/airDataSlice';
 import { useSelector } from 'react-redux';
+import { NavLink } from 'react-router-dom';
 
 import 'styles/HomeBody.css';
 
@@ -9,6 +11,55 @@ const HomeBody = () => {
   const airData = useSelector(selectAirData);
   const loadMessage = <span>Loading</span>;
   const errorMessage = <span className="error">Error</span>;
+  const currLocation = (
+    <span>
+      Information about:&nbsp;
+      {airData?.locationData?.name || ''}
+    </span>
+  );
+  console.log(airData.current);
+  const links = [
+    { path: '/current', text: 'Current Air Quality' },
+    { path: '/forecast', text: 'Forecast Air Quality' },
+    { path: '/historical', text: 'Historical Air Quality' },
+  ];
+
+  const navbar = (
+    links.map((link, i) => {
+      if (i === 0) {
+        return (
+          <li key={nanoid()} className="catItem">
+            <NavLink
+              to={link.path}
+              className={({ isActive }) => (isActive ? 'active' : undefined)}
+            >
+              <p>{link.text}</p>
+              <p>
+                Air Quality Index:&nbsp;
+                {airData?.current[0]?.main?.aqi || ''}
+              </p>
+              <p>
+                Click for more information
+              </p>
+            </NavLink>
+          </li>
+        );
+      }
+      return (
+        <li key={nanoid()} className="catItem">
+          <NavLink
+            to={link.path}
+            className={({ isActive }) => (isActive ? 'active' : undefined)}
+          >
+            <p>{link.text}</p>
+            <p>
+              Click for more information
+            </p>
+          </NavLink>
+        </li>
+      );
+    })
+  );
 
   return (
     <div className="HomeSec">
@@ -18,8 +69,12 @@ const HomeBody = () => {
         <div className={`statusMsg${airData.isLoading || airData.error ? ' modalMsgActiv' : ''}`}>
           {airData.isLoading && loadMessage}
           {airData.error && errorMessage}
+          {!airData.error && !airData.isLoading && currLocation}
         </div>
       </div>
+      <ul className="listCat">
+        {navbar}
+      </ul>
     </div>
   );
 };
