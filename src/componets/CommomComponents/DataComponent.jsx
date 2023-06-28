ba41689c23c9e20f713b602ceba05ffe2cfcb069
Wladimir Pasquel <wladimir.pasquel@urp.edu.pe>
2023-06-28 18:09:25 -0500

    Create DataComponent.jsx

---
+++ src/componets/CommomComponents/DataComponent.jsx
@@ -0,0 +1,20 @@
+import PropTypes from 'prop-types';
+
+const DataComponent = (props) => {
+  const { dataName, dataInfo } = props;
+  const result = dataInfo < 0 ? 'No data' : dataInfo;
+
+  return (
+    <div className="locationWrapper">
+      <p className="dataTitle">{dataName}</p>
+      <p className="dataInfo">{result}</p>
+    </div>
+  );
+};
+
+DataComponent.propTypes = {
+  dataName: PropTypes.string.isRequired,
+  dataInfo: PropTypes.string.isRequired,
+};
+
+export default DataComponent;
