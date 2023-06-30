15052d1ee9b4ed86eea3b5c965265a7a192fa8e9
Wladimir Pasquel <wladimir.pasquel@urp.edu.pe>
2023-06-30 03:56:11 -0500

    Create setupStore for test

--- src/features/store.js
+++ src/features/store.js
@@ -1,6 +1,16 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
 import airDataReducer from 'features/AirPollution/airDataSlice';
 
+const rootReducer = combineReducers({
+  airdata: airDataReducer,
+});
+
+export function setupStore(preloadedState) {
+  return configureStore({
+    reducer: rootReducer,
+    preloadedState,
+  });
+}
 const store = configureStore({
   reducer: {
     airdata: airDataReducer,
